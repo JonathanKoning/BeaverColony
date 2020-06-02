9fa85bf74a53730e1c8717c1efc455d8729b2abb
vellanka <46513529+vellanka@users.noreply.github.com>
2020-06-01 21:54:08 -0700

    Got rid of the placeholders
    Will need to fill with database information
    show all groups that are linked to classes that the student is taking

--- GroupListing.php
+++ GroupListing.php
@@ -5,31 +5,19 @@
     <link rel="stylesheet" href="listingStyle.css">
   </head>
 
-  <h1>Beaver Colony</h1>
-  <h3 style="color:grey;">Course Name</h3>
-  <div class="create">
-      <a href="CreateGroup.php">Create a Group</a>
+  <h1><a href="HomePage.php">Beaver Colony</h1>
+  <h3>Course Name</h3> 
+  <div class="createDiv">
+      <button id="CreateGroupbtn" type="submit" action="CreateGroup.php">Create a Group</a>
   </div>
-
 
   <body>
     <div id="listingBox">
         <div class="listing">
-          <div class="info">
-            #### Day Time Location
-          </div>
-          <div class="info2">
-            0/15
-          </div>
-          <div class="joinButton">
             <form method="get" action="HomePage.php">
-              <button type="submit"> Join</button>
+              <button id="joinButton" type="submit"> Join</button>
             </form>
         </div>
-      </div>
-    </div>
-  </div>
-
-
+   </div>
   </body>
 </html>
