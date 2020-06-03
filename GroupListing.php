72cb71b56b45289e42f3eccc6ac7943b554d8f71
JonathanKoning <jskoning99@gmail.com>
2020-06-03 14:21:19 -0700

    connect goes to Jonathans database. Query is setup but not returning. Probably php syntax thing

--- GroupListing.php
+++ GroupListing.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+    session_start();
+    require_once "connect.php";
+?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -14,6 +19,28 @@
   <body>
     <div id="listingBox">
         <div class="listing">
+            <?php
+                print_r($_SESSION);
+                $name=$_SESSION['onid'];
+                $query = "SELECT * FROM has where ONID='$name'";
+                $Class = mysqli_query($con, $query);
+                $query = "SELECT GroupID FROM participates_in were ONID='$name'";
+                $isin = mysqli_query($con, $query);
+                //$query = "SELECT * FROM Group where (GroupID!=$isin[])";
+
+                $query = "SELECT * FROM Group where ID NOT IN(SELECT GroupID FROM participates_in where ONID = $name) AND Subject IN(SELECT Subject FROM has WHERE ONID=$name) AND SECTION IN(SELECT Section FROM has WHERE ONID=$name) AND Number IN(SELECT Number FROM has WHERE ONID=$name)";
+                $result = mysqli_query($con, $query);
+                print_r($result);
+                while($row = mysqli_fetch_array($result)){
+                    echo"<div class='box'>";
+                    echo"<p>". $row['ID']."<p>";
+                    echo"<form method='get' action='#'>";
+                    echo"<button title='Remove group' id='removeBtn' type='submit'>-</button>";
+                    echo"</form>";
+                    echo"</div>";
+                }
+
+            ?>
             <form method="get" action="HomePage.php">
               <button id="joinButton" type="submit"> Join</button>
             </form>
